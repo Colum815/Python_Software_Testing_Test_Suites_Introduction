7a1fcd241d902f249e002f292752cde921e56a3c
Colum <56079750+Cullie815@users.noreply.github.com>
2021-12-13 16:03:55 +0000

    Update test_suites.py

--- test_suites.py
+++ test_suites.py
@@ -5,6 +5,18 @@ First I create a suite then I decided to add just one test tot the suite by usin
 Then I decided to add the rest using '.addTests()'.
 
 I then run the tests in the terminal using unittest.TextTestRunner().run(suite)
+
+To run the suite in the terminal I wrote down these steps:
+
+1 Include if __name__ = '__main__':
+              unittest.main()
+  in the python file so code can be printed out in the terminal.
+  Open folder where the file you want is. Go to properties and copy location
+2 Open Terminal which will be in a default directory.
+3 cd into the new location to create the new directory with the file in it.
+4 You are in the folder now. To run a file invoke the python interpreter by writing
+  python 'space' then the name of the file with the .py extension.
+5 After .py extension you can add 'space' -v for verbose mode which shows you clearer info on tests run.
 """
 import unittest
 class TestSubtraction(unittest.TestCase):
